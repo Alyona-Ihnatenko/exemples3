527c2d1b4c50277bb8de448ad21729b09acc74cb
Alyona Ihnatenko <altaig4@gmail.com>
2021-04-11 10:27:28 +0300

    hw-last-day

---
+++ js/module-3-26.js
@@ -0,0 +1,7 @@
+function calculateMeanTemperature(forecast) {
+  
+    const {today:{low:todayLow,high:todayHigh},tomorrow:{low:tomorrowLow,high:tomorrowHigh}}=forecast;
+      // Пиши код выше этой строки
+      return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+    }
+    
